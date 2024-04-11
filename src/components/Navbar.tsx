976e96cea196b769eee0aca858c4b69726c541e8
MajdAlkhalaf12 <majd03.hussain@gmail.com>
2024-04-11 22:47:40 +0300

    Logo Changed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/react.svg";
+import logo from "../assets/image.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const Navbar = () => {
   return (
     <HStack justifyContent={"space-between"} padding={"20px"}>
-      <Image src={logo} boxSize="60px"></Image>
+      <Image src={logo} boxSize="70px"></Image>
       <ColorModeSwitch />
     </HStack>
   );
